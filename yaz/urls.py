fa5a33929c65b88558b8044905a7c1301cb70f3c
Sajjad <sajjaddmz@gmail.com>
2025-08-28 01:39:42 +0330

    adding temporary home page to urls

--- yaz/urls.py
+++ yaz/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/dict/', include('dict.urls')),
     path('api/utils/', include('yaz_utils.urls')),  # API base
