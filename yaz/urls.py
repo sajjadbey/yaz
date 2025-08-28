c348cd185a09a9fcf9c54e76e0bc520bbe88528b
Sajjad <sajjaddmz@gmail.com>
2025-08-29 02:34:08 +0330

    live converter

--- yaz/urls.py
+++ yaz/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+    path("convert/", views.convert_page, name="convert_page"),
     path('api/dict/', include('dict.urls')),
     path('api/utils/', include('yaz_utils.urls')),  # API base
 ]
---
+++ yaz/views.py
@@ -0,0 +1,5 @@
+# views.py
+from django.shortcuts import render
+
+def convert_page(request):
+    return render(request, "convert.html")
