f7cdc0892ba1b7c0f13c79707fc268a97fa38ea1
Sajjad <sajjaddmz@gmail.com>
2025-08-29 04:04:45 +0330

    adding description to word model

--- dict/models.py
+++ dict/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Word(models.Model):
     latin = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
     arabic_script = models.CharField(max_length=100, blank=True)
     old_turkic = models.CharField(max_length=100, blank=True)
     translation_en = models.TextField(blank=True)
